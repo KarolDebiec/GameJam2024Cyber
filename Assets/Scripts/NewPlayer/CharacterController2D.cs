8e9f96594b6a5e2ccfe6b76cd831904a76dea760
Karol D <karold@tutanota.com>
2024-05-10 22:47:58 +0200

    god help us all

--- Assets/Scripts/NewPlayer/CharacterController2D.cs
+++ Assets/Scripts/NewPlayer/CharacterController2D.cs
@@ -17,6 +17,14 @@ public class CharacterController2D : MonoBehaviour
     private bool m_FacingRight = true;
     private Vector3 m_Velocity = Vector3.zero;
 
+    public float runSpeed = 40f;
+    private float horizontalMove = 0f;
+
+    public float jumpAnticipationFactor = 0.05f;
+    public float jumpAnticipationTime = 0f;
+    public bool isAnticipating = false;
+
+
     [SerializeField] private float m_MaxSpeed = 10f; // Maksymalna prêdkoœæ biegu
     [SerializeField] private float m_Acceleration = 20f; // Przyspieszenie postaci
 
@@ -33,9 +41,30 @@ public class CharacterController2D : MonoBehaviour
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
     }
-
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(!m_Grounded)
+            {
+                jumpAnticipationTime = 0;
+                isAnticipating = true;
+            }
+            Move(horizontalMove * Time.fixedDeltaTime, false, true);
+        }
+        if(isAnticipating)
+        {
+            jumpAnticipationTime += Time.deltaTime;
+            if(jumpAnticipationTime >= jumpAnticipationFactor)
+            {
+                isAnticipating = false;
+            }
+        }
+    }
     private void FixedUpdate()
     {
+        Move(horizontalMove * Time.fixedDeltaTime, false, false);
         bool wasGrounded = m_Grounded;
         m_Grounded = false;
 
@@ -49,14 +78,58 @@ public class CharacterController2D : MonoBehaviour
                 {
                     gameObject.GetComponent<PlayerMovement>().OnLanding();
                     OnLandEvent.Invoke();
+                    if (isAnticipating)
+                    {
+                        Debug.Log("on grounded : " + m_JumpForce);
+                        m_Grounded = false;
+                        m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
+                        m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                        ResetAnticipation();
+                    }
                 }
             }
         }
     }
+    public void ResetAnticipation()
+    {
+        isAnticipating = false;
+    }
 
     public void Move(float move, bool crouch, bool jump)
-    {
-        if (!crouch && Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+	{
+		// If crouching, check to see if the character can stand up
+		if (!crouch)
+		{
+			// If the character has a ceiling preventing them from standing up, keep them crouching
+			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+			{
+				crouch = true;
+			}
+		}
+
+		//only control the player if grounded or airControl is turned on
+		if (m_Grounded || m_AirControl)
+		{
+
+			// Move the character by finding the target velocity
+			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+			// And then smoothing it out and applying it to the character
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+			// If the input is moving the player right and the player is facing left...
+			if (move > 0 && !m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+		}
+		if(!m_Grounded && jump)
         {
             crouch = true;
         }
@@ -97,8 +170,7 @@ public class CharacterController2D : MonoBehaviour
             m_Grounded = false;
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
         }
-    }
-
+	}
 
     private void Flip()
     {
