fe444951b28d68b1b7819c8ed70b1ebdec7d9448
Wiktor <73667681+MoonGuyPro@users.noreply.github.com>
2024-05-10 23:15:45 +0200

    feat: PlayerCombat

--- Assets/Scripts/NewPlayer/PlayerCombat.cs
+++ Assets/Scripts/NewPlayer/PlayerCombat.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRange = 0.5f;
+    [SerializeField] private LayerMask enemyLayer;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
         
+    }
+
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            //Logika zabierania zycia
+            Debug.Log("hit");
+        }
     }
 }
