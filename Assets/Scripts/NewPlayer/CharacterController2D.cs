3d86a739ac0d51dbe44a62a6a7e277c6b706e4d6
Wiktor <73667681+MoonGuyPro@users.noreply.github.com>
2024-05-10 22:15:17 +0200

    feat: CharacterController change

--- Assets/Scripts/NewPlayer/CharacterController2D.cs
+++ Assets/Scripts/NewPlayer/CharacterController2D.cs
@@ -3,145 +3,108 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-	[Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   // How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
-	[SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
-	[SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
+    [SerializeField] private float m_JumpForce = 400f;
+    [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;
+    [SerializeField] private bool m_AirControl = false;
+    [SerializeField] private LayerMask m_WhatIsGround;
+    [SerializeField] private Transform m_GroundCheck;
+    [SerializeField] private Transform m_CeilingCheck;
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	public bool m_Grounded;            // Whether or not the player is grounded.
-	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
-	private Rigidbody2D m_Rigidbody2D;
-	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-	private Vector3 m_Velocity = Vector3.zero;
+    const float k_GroundedRadius = .2f;
+    public bool m_Grounded;
+    const float k_CeilingRadius = .2f;
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;
+    private Vector3 m_Velocity = Vector3.zero;
 
-	public float jumpAnticipationFactor = 0.05f;
-	public float anticipationTime = 0f;
-	public bool isAnticipating = false;
+    [SerializeField] private float m_MaxSpeed = 10f; // Maksymalna prêdkoœæ biegu
+    [SerializeField] private float m_Acceleration = 20f; // Przyspieszenie postaci
 
-	[Header("Events")]
-	[Space]
+    [Header("Events")]
+    public UnityEvent OnLandEvent;
 
-	public UnityEvent OnLandEvent;
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
 
-	[System.Serializable]
-	public class BoolEvent : UnityEvent<bool> { }
-
-
-	private void Awake()
-	{
-		m_Rigidbody2D = GetComponent<Rigidbody2D>();
-
-		if (OnLandEvent == null)
-			OnLandEvent = new UnityEvent();
-
-	}
-    private void Update()
+    private void Awake()
     {
-        if(isAnticipating)
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = m_Grounded;
+        m_Grounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
         {
-			anticipationTime += Time.deltaTime;
-			if(anticipationTime >= jumpAnticipationFactor)
+            if (colliders[i].gameObject != gameObject)
             {
-				ResetAnticipation();
-			}
+                m_Grounded = true;
+                if (!wasGrounded)
+                {
+                    gameObject.GetComponent<PlayerMovement>().OnLanding();
+                    OnLandEvent.Invoke();
+                }
+            }
         }
     }
-	public void ResetAnticipation()
+
+    public void Move(float move, bool crouch, bool jump)
     {
-		anticipationTime = 0;
-		isAnticipating = false;
-	}
-    private void FixedUpdate()
-	{
-		bool wasGrounded = m_Grounded;
-		m_Grounded = false;
-
-		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				m_Grounded = true;
-				if (!wasGrounded)
-				{
-					gameObject.GetComponent<PlayerMovement>().OnLanding();
-					OnLandEvent.Invoke();
-					if (isAnticipating)
-					{
-						Debug.Log("god help me");
-						m_Grounded = false;
-						m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-						ResetAnticipation();
-					}
-				}
-			}
-		}
-	}
-
-
-	public void Move(float move, bool crouch, bool jump)
-	{
-		// If crouching, check to see if the character can stand up
-		if (!crouch)
-		{
-			// If the character has a ceiling preventing them from standing up, keep them crouching
-			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-			{
-				crouch = true;
-			}
-		}
-
-		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl)
-		{
-
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-		}
-		if(!m_Grounded && jump)
+        if (!crouch && Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
         {
-			ResetAnticipation();
-			isAnticipating = true;
+            crouch = true;
         }
-		// If the player should jump...
-		if (m_Grounded && jump)
-		{
-			// Add a vertical force to the player.
-			m_Grounded = false;
-			Debug.Log("they do be jumpy");
-			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-		}
-	}
+
+        if (m_Grounded || m_AirControl)
+        {
+            // Przyspieszenie i wyhamowanie postaci
+            float targetSpeed = move * m_MaxSpeed;
+            float speedDif = targetSpeed - m_Rigidbody2D.velocity.x;
+            // Zmodyfikuj wartoœæ przyspieszenia w zale¿noœci od kierunku zmiany prêdkoœci
+            float accelRate;
+            if (Mathf.Abs(targetSpeed) > 0.01f)
+            {
+                // Przyspieszanie
+                accelRate = m_Acceleration;
+            }
+            else
+            {
+                // Zwalnianie - u¿yj wiêkszej wartoœci dla szybszego zatrzymywania
+                accelRate = m_Acceleration * 2f; // Mo¿na dostosowaæ, aby zwalnianie by³o szybsze
+            }
+
+            Vector3 targetVelocity = new Vector2(m_Rigidbody2D.velocity.x + speedDif * accelRate * Time.fixedDeltaTime, m_Rigidbody2D.velocity.y);
+            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+            if (move > 0 && !m_FacingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && m_FacingRight)
+            {
+                Flip();
+            }
+        }
+
+        if (m_Grounded && jump)
+        {
+            m_Grounded = false;
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
 
 
-	private void Flip()
-	{
-		// Switch the way the player is labelled as facing.
-		m_FacingRight = !m_FacingRight;
-
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
